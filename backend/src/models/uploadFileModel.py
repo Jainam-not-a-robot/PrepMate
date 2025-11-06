af85d49341dc91877df5fea1f0eecd6e665a32bb
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2025-11-06 19:25:28 +0530

    Edited models

--- backend/src/models/uploadFileModel.py
+++ backend/src/models/uploadFileModel.py
@@ -1,22 +1,27 @@
-from sqlalchemy import Text, Integer, Column, Boolean
+from sqlalchemy import Text, Integer, Column, Boolean , func , ForeignKey , DateTime
+from sqlalchemy.orm import relationship
 from ..db.database import Base
 
-class fileUploadModel(Base):
+class FileUploadModel(Base):
     __tablename__="filesuploaded"
     id=Column(Integer,primary_key=True,index=True)
     filename=Column(Text,nullable=False)
 
-class checklistModel(Base):
-    __tablename__="checklist"
-    checklist_id=Column(Integer,primary_key=True,index=True)
-    item=Column(Text,nullable=False)
-    isCompleted=(Boolean,default=False)
-
-class multipleChecklistModel(Base):
+class MultipleChecklistModel(Base):
     __tablename__="multichecklist"
     item_id=Column(Integer,primary_key=True,index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    checklists=relationship("ChecklistModel",back_populates="parent",cascade="all, delete-orphan")
 
-class quizModel(Base):
+class ChecklistModel(Base):
+    __tablename__="checklist"
+    checklist_id=Column(Integer,primary_key=True,index=True)
+    item_id = Column(Integer, ForeignKey("multichecklist.item_id"),nullable=False)
+    item=Column(Text,nullable=False)
+    isCompleted=Column(Boolean,default=False)
+    parent = relationship("MultipleChecklistModel", back_populates="checklists")
+
+class QuizModel(Base):
     __tablename__="quiz"
     question_id=Column(Integer,primary_key=True,index=True)
     question=Column(Text,nullable=False)
